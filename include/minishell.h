02076f122502fb28d1fa534b46bc00985d853809
hicham <hicham@gmaile.com>
2025-04-27 14:45:23 +0100

    hello iam hicham

--- include/minishell.h
+++ include/minishell.h
@@ -1,17 +1,38 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-#include <readline/history.h>
+
 #include <readline/readline.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <readline/history.h>
+#include <unistd.h>
 #include "../libft/libft.h"
+#include <stdbool.h>
+
+typedef enum s_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_REDIR_APPEND,
+	TOKEN_HERDOC
+}	t_token_type;
+
+typedef struct s_token
+{
+	char	*av;
+	t_token_type	type;
+	struct s_data *next;
+}	t_token;
 
 typedef struct s_data
 {
-	char	**av;
-	struct s_data *next;
+	int start;
+	bool	in_quot;
+	char	quot_char;
 }	t_data;
 
 void	make_prompt();
--- parser.c
+++ parser.c
@@ -1,47 +1,74 @@
 #include "include/minishell.h"
 #include "libft/libft.h"
-#include <stdio.h>
+#include <readline/readline.h>
+#include <signal.h>
 
-char	*sub_t(char *line)
+t_token *creat_token(char *line, t_token_type type)
 {
-	int i = 0;
-	int len = 0;
-	while (line[i])
-	{
-		if (line[i] != '"')
-		{
-			len++;
-		}
-		i++;
-	}
-	char *buffer = malloc((len + 1) * sizeof(char));
-	if (!buffer)
-	{
+	t_token *new_token;
+
+	if (!(new_token = (t_token *)ft_calloc(0, sizeof(t_token))))
 		return (NULL);
-	}
-	i = 0;
-	int j = 0;
-	while(line[i])
-	{
-		if (line[i] != '"')
-		{
-			buffer[j] = line[i];
-			j++;
-		}
-		i++;
-	}
-	buffer[i] = '\0';
-	return (buffer);
+	if (!(new_token->av = ft_strdup(word)))
+		return (free(new_token), NULL);
+	new_token->type = type;
+	new_token->next = NULL;
+	return (new_token);
 }
 
-void	my_split(char *line)
+static void	handle_quote(bool	*in_quot ,char *quot_char, int *i, char *line)
 {
-	char **av = ft_split(line, ' ');
-	int i = 0;
-	while (av[i])
+	if ((line[*i] == '"' || line[*i] == '\''))
 	{
-		printf ("%s\n", av[i]);
+		if (*quot_char == '\0')
+		{
+			*in_quot = true;
+			*quot_char = line[*i];
+		}
+		else if (*quot_char == line[*i])
+		{
+			*in_quot = false;
+			*quot_char = 0;
+		}
+	}
+}
+
+t_token	*tokenize(char *line)
+{
+	int i;
+	t_token	*token = NULL;
+	bool	in_quot = false;
+	char	quot_char = 0;
+	int start = 0;
+	char *word;
+
+	i = 0;
+	while (line[i])
+	{
+	 	handle_quote(&in_quot, &quot_char, &i, line);
+		if (i > start)
+		{
+			if (!(word = ft_substr(line, start, i - start)))
+				return (NULL);
+			if (!(add_token(&token, creat_token(word, TOKEN_WORD))))
+				return (free(word), NULL);
+			free(word);
+
+		}
 		i++;
+	}
+	return (token);
+}
+
+
+void	my_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 	}
 }
 
@@ -49,13 +76,14 @@ void make_prompt()
 {
     char *line;
     char *prompt;
-	char *print;
+	t_token	*token;
     
     prompt = ft_strjoin(getenv("HOME"), "@minishell$ ");
     if (!prompt)
 		return;
     while(1)
     {
+		signal(SIGINT, my_handler);
         line = readline(prompt);
         
         if (!line)
@@ -63,12 +91,11 @@ void make_prompt()
             printf("exit\n");
             break;
         }
-        if (line)
+        if (line[0] != '\0')
         {
             add_history(line);
+			token = tokenize(line);
         }
-		print = sub_t(line);
-		my_split(print);
         free(line);
     }
     free(prompt);
